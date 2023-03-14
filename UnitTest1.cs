07bf1f35ccc2178827e289744a26df2309545d95
Nikhlesh Rao <38323670+Nikhlesh117@users.noreply.github.com>
2023-03-14 08:48:15 +0530

    Added: Calculate Fare

---
+++ UnitTest1.cs
@@ -0,0 +1,21 @@
+using CabInvoiceGenerator;
+namespace NUnitInvoiceGeneratorTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+
+        InvoiceGenerator invoiceGenerator = null;
+
+        [TestMethod]
+        public void GivenDistanceAndTimeShouldReturnTotalFare()
+        {
+            invoiceGenerator = new InvoiceGenerator(RideType.NORMAL);
+            double distance = 2.0;
+            int time = 5;
+            double fare = invoiceGenerator.CalculateFare(distance, time);
+            double expected = 25;
+            Assert.AreEqual(expected, fare);
+        }
+    }
+}
